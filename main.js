08b5791e1eb998c9f994af62515a414731f039bd
Avandahm <112453899+Avandahm@users.noreply.github.com>
2022-08-29 22:21:14 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,16 @@
+var myAge = 27;
+// my age is 27 
+var earlyYears = 2;
+//early years is 2
+earlyYears *= 10.5;
+
+var laterYears = myAge - 2;
+
+laterYears *= 4;
+
+//console.log(earlyYears,lateryears)
+var myAgeInDogYears = earlyYears + laterYears;
+//my age in dog years is my early years plus my later years
+var myName = 'Alex'.toLowerCase();
+//my name lowercase
+console.log(My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.);
